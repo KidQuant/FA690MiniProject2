0cb7532b6260e1699dddaf02c2d6d0fcf079128e
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-04-11 19:56:31 -0400

    Updates
    Updates for project notebook. New additions for the MLP, LSTM, and
    FinBERT LSTM models for seperate python codes.

---
+++ MLP.py
@@ -0,0 +1,126 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_percentage_error
+import tensorflow as tf
+
+# hyperparameters
+split = (0.85);
+sequence_length = 10;
+epochs = 10
+learning_rate = 0.01
+
+
+
+# loading stock price data
+stock_data = pd.read_csv("data/price.csv")
+column = ['close']
+
+
+len_stock_data = stock_data.shape[0]
+
+
+# splitting data to train and test
+train_examples = int(len_stock_data * split)
+train = stock_data.get(column).values[:train_examples]
+test = stock_data.get(column).values[train_examples:]
+len_train = train.shape[0]
+len_test = test.shape[0]
+
+
+# normalizing data
+scaler = MinMaxScaler()
+train, test = scaler.fit_transform(train), scaler.fit_transform(test)
+
+
+# splitting training data to x and y
+X_train = []
+for i in range(len_train - sequence_length):
+    X_train.append(train[i : i + sequence_length])
+X_train = np.array(X_train).astype(float)
+y_train = np.array(train[sequence_length:]).astype(float)
+
+# splitting testing data to x and y
+X_test = []
+for i in range(len_test - sequence_length):
+    X_test.append(test[i : i + sequence_length])
+X_test = np.array(X_test).astype(float)
+y_test = np.array(test[sequence_length:]).astype(float)
+
+
+# MLP Model
+def model_create():
+    tf.random.set_seed(1234)
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.Input(shape=(X_train.shape[1], 1)),
+            tf.keras.layers.LSTM(units=50, activation="tanh", return_sequences=True),
+            tf.keras.layers.Dropout(0.15),
+            tf.keras.layers.LSTM(units=30, activation="tanh", return_sequences=True),
+            tf.keras.layers.Dropout(0.05),
+            tf.keras.layers.LSTM(units=20, activation="tanh", return_sequences=False),
+            tf.keras.layers.Dropout(0.01),
+            tf.keras.layers.Dense(units=1, activation="linear"),
+        ]
+    )
+
+    model.compile(
+        loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+    )
+
+    # Fit the model and store the history
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=epochs,
+        validation_split=0.2,
+        verbose=1
+    )
+    return model, history
+
+
+# inverting normaliztion
+y_test = scaler.inverse_transform(y_test)
+
+
+# prediction on test set
+def predict(model):
+    predictions = model.predict(X_test)
+    predictions = scaler.inverse_transform(predictions.reshape(-1, 1)).reshape(-1, 1)
+    return predictions
+
+
+# evaluation
+def evaluate(predictions):
+    mae = mean_absolute_error(predictions, y_test)
+    mape = mean_absolute_percentage_error(predictions, y_test)
+    r2 = calculate_r2(y_test, predictions, in_sample=False, benchmark = 0)
+    return mae, mape, (1 - mape), r2
+
+
+def run_model(n):
+
+    total_mae = total_mape = total_acc =total_r2 = 0
+    histories = []
+
+    for i in range(n):
+        model, history = model_create()
+        predictions = predict(model)
+        mae, mape, acc, r2 = evaluate(predictions)
+        total_mae += mae
+        total_mape += mape
+        total_acc += acc
+        total_r2 += r2
+        histories.append(history)
+
+    return (total_mae / n), (total_mape / n), (total_acc / n), histories, predictions.tolist(), (total_r2/n)
+
+mae, mape, acc, preds, histories, r2 = run_model(1)
+
+
+print(f"Mean Absolute Error = {mae}")
+print(f"Mean Absolute Percentage Error = {mape}%")
+print(f"Accuracy = {acc}")
+print(f"R2 Score = {r2}")
